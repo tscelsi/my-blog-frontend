142882821c5f5a8344499670012ede5506f7d8e3
Thomas Scelsi <tscelsi@evenergi.com>
2025-06-10 16:29:58 +0200

    fix: border on memory list

--- src/components/MemoryList.tsx
+++ src/components/MemoryList.tsx
@@ -13,11 +13,13 @@ export const MemoryList = ({ memories }: MemoryListProps) => {
   return (
     <div className="flex flex-col">
       {memories.map((memory) => (
-        <Link key={memory.id} to="/$memoryId" params={{ memoryId: memory.id }}>
-          <div
-            className="hover:opacity-80 cursor-pointer border-b border-dark-grey first:border-t"
-            onClick={() => setActiveMemory(memory)}
-          >
+        <Link
+          className="hover:opacity-80 cursor-pointer border-b border-dark-grey first:border-t"
+          key={memory.id}
+          to="/$memoryId"
+          params={{ memoryId: memory.id }}
+        >
+          <div onClick={() => setActiveMemory(memory)}>
             <p className="mx-6 md:mx-8">
               {formatDate(memory.created_at)}: {memory.title}
             </p>
